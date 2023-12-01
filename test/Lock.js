277cbea29dcbe25973dce8b94ccd7654790e26fe
Houssam LAGHZIL <houssam.laghzil@edu.itescia.fr>
2023-12-01 16:52:50 +0100

    test

--- test/Lock.js
+++ test/Lock.js
@@ -1,26 +1,41 @@
-import {loadFixture, time} from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import {ethers} from "hardhat";
-import {expect} from "chai";
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const {ethers} = require("hardhat");
+const {expect} = require("chai");
 
 describe("Counter", async function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployCounterFixture() {
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners();
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshot in every test.
+    async function deployCrowdsaleFixture() {
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
 
-    const Crowdsale = await ethers.getContractFactory("Crowdsale");
-    const crowdsale = await Crowdsale.deploy(1,5,0.2,);
+        const ERC20 = await ethers.getContractFactory("MYTOKEN");
+        const Crowdsale = await ethers.getContractFactory("Crowdsale");
 
-    return { counter: crowdsale, owner, otherAccount };
-  }
+        const erc20 = await ERC20.deploy();
+        const crowdsale = await Crowdsale.deploy(10,10,1,erc20.getAddress());
+        erc20.transfer(crowdsale.getAddress(), 1000000);
 
-  it("Should stay at zero", async function () {
-    const { counter} = await loadFixture(deployCounterFixture);
+        return { crowdsale, owner, otherAccount };
+    }
 
-    await counter.upCount()
-    const count = await counter.getCount();
-    expect(count).to.equal(0)
-  });
+    it('tentative d achat', async () => {
+        const [owner, otherAccount] = await ethers.getSigners();
+        const {crowdsale} = await loadFixture(deployCrowdsaleFixture);
+        await crowdsale.connect(otherAccount).buy(
+            {value: 1}
+        );
+        expect(await crowdsale.balanceOf(otherAccount.address)).to.equal(1);
+    });
+
+    it('tentative d achat sans avoir assez de token', async () => {
+        const [owner, otherAccount] = await ethers.getSigners();
+        const {crowdsale} = await loadFixture(deployCrowdsaleFixture);
+        await expect(crowdsale.connect(otherAccount).buy(
+            {value: ethers.parseUnits("10000000000000000001","wei")}
+        )).to.be.reverted;
+    }
+    );
+
 })
