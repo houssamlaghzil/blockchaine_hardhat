0b232fe64cd913b1820962e98b239b6a52eec1ed
Houssam LAGHZIL <houssam.laghzil@edu.itescia.fr>
2023-12-01 14:24:51 +0100

    Crowsales done

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -1,53 +1,86 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-interface token {
-    function transfer(address receiver, uint amount) external;
-}
+// Importe le contrat Ownable d'OpenZeppelin
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Crowdsale {
-    address public beneficiary;
-    uint public fundingGoal;
-    uint public amountRaised;
-    uint public deadline;
-    uint public price;
-    token public tokenReward;
-    mapping(address => uint256) public balanceOf;
-    bool fundingGoalReached = false;
-    bool crowdsaleClosed = false;
+// Définit le contrat Crowdsale, qui est Ownable
+contract Crowdsale is Ownable {
+    // Déclare des variables d'état
+    uint public fundingGoal;             // Objectif de collecte de fonds en Ether
+    uint public amountRaised;            // Montant total collecté jusqu'à présent
+    uint public deadline;                // Date limite de la campagne de financement
+    uint public price;                   // Prix d'un jeton en Ether
+    IERC20 public tokenReward;           // Adresse du contrat du jeton utilisé comme récompense
+    mapping(address => uint256) public balanceOf; // Solde de chaque contributeur
+    bool public fundingGoalReached = false;       // Indique si l'objectif de collecte a été atteint
+    bool public crowdsaleClosed = false;          // Indique si la campagne de financement est close
 
+    // Événements pour enregistrer des logs
     event GoalReached(address recipient, uint totalAmountRaised);
     event FundTransfer(address backer, uint amount, bool isContribution);
 
+    // Constructeur pour initialiser le contrat Crowdsale, en appelant également le constructeur Ownable
     constructor(
         address ifSuccessfulSendTo,
         uint fundingGoalInEthers,
         uint durationInMinutes,
         uint etherCostOfEachToken,
         address addressOfTokenUsedAsReward
-    ) public {
+    ) Ownable() {
         beneficiary = ifSuccessfulSendTo;
         fundingGoal = fundingGoalInEthers * 1 ether;
-        deadline = now + durationInMinutes * 1 minutes;
+        deadline = block.timestamp + durationInMinutes * 1 minutes;
         price = etherCostOfEachToken * 1 ether;
-        tokenReward = token(addressOfTokenUsedAsReward);
+        tokenReward = IERC20(addressOfTokenUsedAsReward);
     }
 
+    // Fonction fallback pour recevoir de l'Ether et distribuer des jetons
     function () payable public {
+        // Assurez-vous que la campagne de financement est toujours ouverte
         require(!crowdsaleClosed);
+
+        // Enregistre la contribution de l'expéditeur
         uint amount = msg.value;
         balanceOf[msg.sender] += amount;
         amountRaised += amount;
+
+        // Transfère des jetons à l'expéditeur en fonction de l'Ether envoyé
         tokenReward.transfer(msg.sender, amount / price);
+
+        // Émet un événement pour enregistrer le transfert de fonds
         emit FundTransfer(msg.sender, amount, true);
     }
 
-    modifier afterDeadline() { if (now >= deadline) _; }
+    // Modificateur pour vérifier si le temps actuel est après la date limite de financement
+    modifier afterDeadline() {
+        if (block.timestamp >= deadline)
+            _;
+    }
 
+    // Fonction pour vérifier si l'objectif de collecte a été atteint après la date limite
     function checkGoalReached() public afterDeadline {
         if (amountRaised >= fundingGoal){
             fundingGoalReached = true;
+
+            // Émet un événement pour enregistrer l'atteinte de l'objectif
             emit GoalReached(beneficiary, amountRaised);
         }
     }
-}
 
+    // Fonction pour retirer les jetons si l'objectif de collecte est atteint
+    function withdrawTokens() public onlyOwner afterDeadline{
+        uint unsoldTokens = tokenReward.balanceOf(address(this)) - amountRaised / price;
+        if (unsoldTokens > 0) {
+            // Transfère tous les jetons invendus au propriétaire
+            tokenReward.transfer(owner(), unsoldTokens);
+        }
+    }
+
+    // Fonction pour retirer les fonds si l'objectif de collecte est atteint
+    function withdrawFunds() public onlyOwner afterDeadline{
+        // Transfère l'intégralité du solde au propriétaire
+        payable(owner()).transfer(address(this).balance);
+    }
+}
--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -3,18 +3,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract MyNFT is ERC20 {
+
     constructor() ERC20("MYTOKEN", "MTKN") {
         _mint(msg.sender, 1000000000000000000000000);
-    }
-
-    function mintMinerReward() public {
-        _mint(block.coinbase, 1000);
-    }
-
-    function _update(address from, address to, uint256 value) internal virtual override {
-        if (!(from == address(0) && to == block.coinbase)) {
-            _mintMinerReward();
-        }
-        super._update(from, to, value);
     }
 }
