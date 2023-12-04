5756bc81943e6e8394954895336d394945cadd30
Houssam LAGHZIL <houssam.laghzil@edu.itescia.fr>
2023-12-04 15:38:41 +0100

    readme + config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
-var task = require("hardhat/config").task;
+const dotenv = require("dotenv");
+dotenv.config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    solidity: "0.8.20",
     networks: {
         mumbai: {
-            url: "https://polygon-mumbai.g.alchemy.com/v2/alcht_WtvbTgX0IGeYXhiWtsTcOyD4PxyiEX",
+            url: "https://rpc-mumbai.maticvigil.com/v1/b6458a5ff27ced3a6270009577979cb15e4fee8e",
             accounts: [process.env.PRIVATE_KEY]
         }
     },
     etherscan: {
-        apiKey: 'alcht_WtvbTgX0IGeYXhiWtsTcOyD4PxyiEX'
-    },
-
-    solidity: "0.8.20"
+        apiKey:process.env.POLYGONSCAN_API_KEY
+    }
 };
-
-// hardhat.config.js
-
-task("verify:get-contract-information", "Get contract information")
-    .addParam("contractAddress", "The address of the deployed contract")
-    .setAction(async ({ contractAddress }, hre) => {
-        const { ethers } = hre;
-
-        // Obtenez l'instance du contrat à partir de l'adresse
-        const contract = await ethers.getContractAt("ContractName", contractAddress);
-
-        // Obtenez des informations spécifiques sur le contrat
-        const contractOwner = await contract.owner();
-        const contractBalance = await ethers.provider.getBalance(contractAddress);
-
-        // Affichez les informations
-        console.log("Contract Owner:", contractOwner);
-        console.log("Contract Balance:", ethers.utils.formatEther(contractBalance));
-    });
-
-
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,33 +5,24 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const {ethers} = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const MYTOKEN = await ethers.getContractFactory("MYTOKEN");
+  const myToken = await MYTOKEN.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await myToken.waitForDeployment();
 
-  const MYTOKEN = await hre.ethers.deployContract("MYTOKEN");
+  // const Crowdsale = await ethers.getContractFactory("Crowdsale", [10,10,1,MYTOKEN.address]);
+  const Crowdsale = await ethers.getContractFactory("Crowdsale");
+  const crowdsale = await Crowdsale.deploy( 10,10,1, await myToken.getAddress());
 
-  await MYTOKEN.waitForDeployment();
+  await crowdsale.waitForDeployment();
 
-  const Crowdsale = await hre.ethers.deployContract("Crowdsale", [10,10,1,MYTOKEN.address]);
-
-  await Crowdsale.waitForDeployment();
-
-  await MYTOKEN.transfer(Crowdsale.address, 1000000);
-
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${MYTOKEN.target}`
-  );
+  await myToken.transfer( await crowdsale.getAddress(), 1000000);
+  console.log("Crowdsale deployed to:", await crowdsale.getAddress());
+  console.log("erc deployed to:", await myToken.getAddress());
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
